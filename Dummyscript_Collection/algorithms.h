55e380d07835537402592f28be0b9991b3553078
Al3k <Al3k@headclash>
2025-05-16 18:51:57 -0300

    - Fixed a bug with the 'triangle' function, in which it wouldn't output anything if the 3rd side wasn't qual to the 1st side.
    - All code reformatted for more a more coherent look.

--- Dummyscript_Collection/algorithms.h
+++ Dummyscript_Collection/algorithms.h
@@ -2,17 +2,17 @@
 #include <iostream>  
 #include "extraFunc.h"  
 
-class Algorithms {  
-public:  
-    // Function to check if a student has a passing grade or if they'll be sent to recovery  
-    void grade();  
-    // Function to check if a triangle is equilateral  
-    void triangle();  
-    // Function to determine if a number is positive, negative, or zero  
-    void number();  
-    // Function to determine the age category of a person  
-    void age();  
-    // Function to provide a chatbot-like interaction with options  
-    void chatbot();  
+class Algorithms {
+public:
+	// Function to check if a student has a passing grade or if they'll be sent to recovery  
+	void grade();
+	// Function to check if a triangle is equilateral  
+	void triangle();
+	// Function to determine if a number is positive, negative, or zero  
+	void number();
+	// Function to determine the age category of a person  
+	void age();
+	// Function to provide a chatbot-like interaction with options  
+	void chatbot();
 
 };
